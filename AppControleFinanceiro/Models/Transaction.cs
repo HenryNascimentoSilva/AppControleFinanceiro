03cc829c05dc87caa72fa14ca543d1239d7e8630
HenryNascimento <henrynascimentosilva@gmail.com>
2023-11-20 23:16:05 -0300

    Model

---
+++ AppControleFinanceiro/Models/Transaction.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppControleFinanceiro.Models
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public TransactionType Type { get; set; }
+
+        public DateTimeOffset Date { get; set; }
+
+        public decimal Value { get; set; }
+    }
+
+    public enum TransactionType
+    {
+        Income,
+        Expenses
+    }
+}
